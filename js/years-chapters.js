b546f300e39789f5ad1d17a315cb8a6e78d94507
SQflex <egorkazgorki2@gmail.com>
2026-01-26 21:49:55 +0300

    add: test url

--- js/years-chapters.js
+++ js/years-chapters.js
@@ -3,7 +3,8 @@
 let currentChapterID = null;
 let currentPlanId = null;
 let currentYear = new Date().getFullYear();
-let baseURL = "http://172.16.251.93:8010";
+// let baseURL = "http://172.16.251.93:8010";
+let baseURL = "http://localhost:8080";
 
 function loadYearsFromPlans(branchId, chapterId, defaultYear) {
 
